647493609fff05fec32bef6d29ed02a3462f974e
Mohammad Hamid Raza <raza.hamid09@gmail.com>
2023-12-29 00:12:07 +0530

    initial commit

--- src/controllers/user/routes.ts
+++ src/controllers/user/routes.ts
@@ -12,7 +12,7 @@ const { MULTER } = config;
 const router = Router();
 
 router
-  .post('/signup', MULTER.single('profile'), userController.signup)
+  .post('/signup', authMiddleware.authenticate, authMiddleware.authorize(ROLES.ADMIN), MULTER.single('profile'), userController.signup)
   .post('/login', userController.login)
   .get('/:username', userController.getUser)
   .get('/', userController.getAllUsers)
